d65768d96e8e89947dada249ed97a273e412f9e6
zhangzhoufei <zhangzhoufei@2144.cn>
2017-04-23 21:26:03 +0800

    frontend starting

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,11 @@
 const { mix } = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.config.detectHotReloading();
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+if (mix.config.inProduction) {
+  mix.version();
+  mix.disableNotifications();
+}
